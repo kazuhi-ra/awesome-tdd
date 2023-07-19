0e23ad8de5aad391c3e3346a86144d0188ee6d61
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-20 03:46:43 +0900

    red: コンポーネントを組み合わせてみる

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -1,64 +1,61 @@
 require_relative "../src/number_converter"
+require_relative "../src/cylic_number_rule"
+require_relative "../src/path_through_rule"
 
 RSpec.describe NumberConverter do
   describe "#convert" do
-    subject { @fizz_buzz.convert(input) }
-    before { @fizz_buzz = NumberConverter.new(rules) }
-
-    context "ruleなし" do
-      let(:rules) { [] }
-
-      context "1の時" do
-        let(:input) { 1 }
-        it { should eq "" }
-      end
+    subject do
+      converter = NumberConverter.new([
+        CylicNumberRule.new(3, "Fizz"),
+        CylicNumberRule.new(5, "Buzz"),
+        PathThroughRule.new(),
+      ])
+      converter.convert(input)
     end
 
-    context "ruleがある" do
-      context "ruleが1つ" do
-        let(:rules) do
-          rule = double("rule")
-          allow(rule).to receive(:replace).with(1).and_return("Replaced")
-          [rule]
-        end
+    context "1の時" do
+      let(:input) { 1 }
+      it { should eq "1" }
+    end
 
-        context "1の時" do
-          let(:input) { 1 }
-          it { should eq "Replaced" }
-        end
-      end
+    context "2の時" do
+      let(:input) { 2 }
+      it { should eq "2" }
+    end
 
-      context "ruleが2つ" do
-        let(:rules) do
-          rule1 = double("rule1")
-          rule2 = double("rule2")
-          allow(rule1).to receive(:replace).with(1).and_return("Fizz")
-          allow(rule2).to receive(:replace).with(1).and_return("Buzz")
-          [rule1, rule2]
-        end
+    context "3の時" do
+      let(:input) { 3 }
+      it { should eq "Fizz" }
+    end
 
-        context "1の時" do
-          let(:input) { 1 }
-          it { should eq "FizzBuzz" }
-        end
-      end
+    context "4の時" do
+      let(:input) { 4 }
+      it { should eq "4" }
+    end
 
-      context "ruleが3つ" do
-        let(:rules) do
-          rule1 = double("rule1")
-          rule2 = double("rule2")
-          rule3 = double("rule3")
-          allow(rule1).to receive(:replace).with(1).and_return("")
-          allow(rule2).to receive(:replace).with(1).and_return("")
-          allow(rule3).to receive(:replace).with(1).and_return("1")
-          [rule1, rule2, rule3]
-        end
+    context "5の時" do
+      let(:input) { 5 }
+      it { should eq "Buzz" }
+    end
 
-        context "1の時" do
-          let(:input) { 1 }
-          it { should eq "1" }
-        end
-      end
+    context "6の時" do
+      let(:input) { 6 }
+      it { should eq "Fizz" }
+    end
+
+    context "10の時" do
+      let(:input) { 10 }
+      it { should eq "Buzz" }
+    end
+
+    context "15の時" do
+      let(:input) { 15 }
+      it { should eq "FizzBuzz" }
+    end
+
+    context "30の時" do
+      let(:input) { 30 }
+      it { should eq "FizzBuzz" }
     end
   end
 end
