598269097763950cb73e5b9b08c6db57ecdbb02f
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-20 00:51:49 +0900

    red

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe NumberConverter do
     it "1の時" do
       expect(@fizz_buzz.converter 1).to eq 1
     end
+
+    it "2の時" do
+      expect(@fizz_buzz.converter 2).to eq 2
+    end
   end
 end
