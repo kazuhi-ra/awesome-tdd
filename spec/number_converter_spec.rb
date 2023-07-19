0133aebe1740c10904d3518f0dbe8e7b877531ce
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-20 00:53:53 +0900

    red: 返り値は文字列を期待する

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe NumberConverter do
 
   describe "#converter" do
     it "1の時" do
-      expect(@fizz_buzz.converter 1).to eq 1
+      expect(@fizz_buzz.converter 1).to eq "1"
     end
 
     it "2の時" do
-      expect(@fizz_buzz.converter 2).to eq 2
+      expect(@fizz_buzz.converter 2).to eq "2"
     end
   end
 end
