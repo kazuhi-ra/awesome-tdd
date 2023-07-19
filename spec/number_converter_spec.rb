488d517696078f082e012edb24e85183f89bd052
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-20 02:52:32 +0900

    red: #replaceを持つruleをスタブしテスト

--- spec/number_converter_spec.rb
+++ spec/number_converter_spec.rb
@@ -12,5 +12,18 @@ RSpec.describe NumberConverter do
         it { should eq "" }
       end
     end
+
+    context "ruleがある" do
+      let(:rules) do
+        rule = double("rule")
+        allow(rule).to receive(:replace).with(1).and_return("Replaced")
+        [rule]
+      end
+
+      context "1の時" do
+        let(:input) { 1 }
+        it { should eq "Replaced" }
+      end
+    end
   end
 end
