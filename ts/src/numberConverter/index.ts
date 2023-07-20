fee699b8101a467cee87299541e89e37963ba5d3
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-21 00:22:29 +0900

    green: 3の倍数ときはFizzが返る

--- ts/src/numberConverter/index.ts
+++ ts/src/numberConverter/index.ts
@@ -1,9 +1,9 @@
 export class NumberConverter {
   convert(n: number): string {
-    if (n === 3) {
+    if (n % 3 === 0) {
       return 'Fizz'
     } else {
-      if (n === 5) {
+      if (n % 5 === 0) {
         return 'Buzz'
       } else {
         return `${n}`
