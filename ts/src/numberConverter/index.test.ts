18b567eb2eee47d15f19e0bbf7dbe735c013e25b
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-21 00:24:57 +0900

    この辺で整えておく

--- ts/src/numberConverter/index.test.ts
+++ ts/src/numberConverter/index.test.ts
@@ -4,6 +4,20 @@ describe('NumberConverter', () => {
   describe('convert', () => {
     const fizzBuzz = new NumberConverter()
 
+    describe('3の倍数とき', () => {
+      it('Fizzが返る', () => {
+        expect(fizzBuzz.convert(3)).toBe('Fizz')
+        expect(fizzBuzz.convert(6)).toBe('Fizz')
+      })
+    })
+
+    describe('5の倍数のとき', () => {
+      it('Buzzが返る', () => {
+        expect(fizzBuzz.convert(5)).toBe('Buzz')
+        expect(fizzBuzz.convert(10)).toBe('Buzz')
+      })
+    })
+
     describe('1のとき', () => {
       it('数値が文字列になって返る', () => {
         expect(fizzBuzz.convert(1)).toBe('1')
@@ -16,33 +30,9 @@ describe('NumberConverter', () => {
       })
     })
 
-    describe('3のとき', () => {
-      it('Fizzが返る', () => {
-        expect(fizzBuzz.convert(3)).toBe('Fizz')
-      })
-    })
-
     describe('4のとき', () => {
       it('Fizzが返る', () => {
         expect(fizzBuzz.convert(4)).toBe('4')
-      })
-    })
-
-    describe('5のとき', () => {
-      it('Buzzが返る', () => {
-        expect(fizzBuzz.convert(5)).toBe('Buzz')
-      })
-    })
-
-    describe('6のとき', () => {
-      it('Fizzが返る', () => {
-        expect(fizzBuzz.convert(6)).toBe('Fizz')
-      })
-    })
-
-    describe('10のとき', () => {
-      it('Buzzが返る', () => {
-        expect(fizzBuzz.convert(10)).toBe('Buzz')
       })
     })
   })
