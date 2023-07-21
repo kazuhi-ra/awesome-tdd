faac8ffb046e1af1eb90b22a62fd0300a6224f9f
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-21 12:04:44 +0900

    red: 5のときはBuzzが返る

--- ts/src/replaceRules/cyclicNumberRule.ts
+++ ts/src/replaceRules/cyclicNumberRule.ts
@@ -2,7 +2,11 @@ import { ReplaceRule } from '.'
 
 class CyclicNumberRule implements ReplaceRule {
   replace(n: number): string {
-    return ''
+    if (n % 3 === 0) {
+      return 'Fizz'
+    } else {
+      return ''
+    }
   }
 }
 
@@ -14,6 +18,13 @@ if (import.meta.vitest) {
         expect(fizzRule.replace(1)).toBe('')
         expect(fizzRule.replace(3)).toBe('Fizz')
         expect(fizzRule.replace(666)).toBe('Fizz')
+      })
+
+      it('5の倍数でBuzzと返す', () => {
+        const fizzRule = new CyclicNumberRule()
+        expect(fizzRule.replace(1)).toBe('')
+        expect(fizzRule.replace(5)).toBe('Buzz')
+        expect(fizzRule.replace(555)).toBe('Buzz')
       })
     })
   })
