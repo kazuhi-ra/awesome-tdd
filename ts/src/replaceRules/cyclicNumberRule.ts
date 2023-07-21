f3173216e9d16d910b06d2c4d89b5cc3b7ab4f1f
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-21 12:05:36 +0900

    green: 5のときはBuzzが返る

--- ts/src/replaceRules/cyclicNumberRule.ts
+++ ts/src/replaceRules/cyclicNumberRule.ts
@@ -1,9 +1,17 @@
 import { ReplaceRule } from '.'
 
-class CyclicNumberRule implements ReplaceRule {
+export class CyclicNumberRule implements ReplaceRule {
+  #base: number
+  #replacement: string
+
+  constructor(base: number, replacement: string) {
+    this.#base = base
+    this.#replacement = replacement
+  }
+
   replace(n: number): string {
-    if (n % 3 === 0) {
-      return 'Fizz'
+    if (n % this.#base === 0) {
+      return this.#replacement
     } else {
       return ''
     }
@@ -14,17 +22,17 @@ if (import.meta.vitest) {
   describe('CyclicNumberRule', () => {
     describe('replace', () => {
       it('3の倍数でFizzと返す', () => {
-        const fizzRule = new CyclicNumberRule()
+        const fizzRule = new CyclicNumberRule(3, 'Fizz')
         expect(fizzRule.replace(1)).toBe('')
         expect(fizzRule.replace(3)).toBe('Fizz')
         expect(fizzRule.replace(666)).toBe('Fizz')
       })
 
       it('5の倍数でBuzzと返す', () => {
-        const fizzRule = new CyclicNumberRule()
-        expect(fizzRule.replace(1)).toBe('')
-        expect(fizzRule.replace(5)).toBe('Buzz')
-        expect(fizzRule.replace(555)).toBe('Buzz')
+        const buzzRule = new CyclicNumberRule(5, 'Buzz')
+        expect(buzzRule.replace(1)).toBe('')
+        expect(buzzRule.replace(5)).toBe('Buzz')
+        expect(buzzRule.replace(555)).toBe('Buzz')
       })
     })
   })
