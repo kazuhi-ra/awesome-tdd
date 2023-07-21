55694d2e3ebd02d935d49c5747ec8d6cd43d4475
kazuhi-ra <kazuhira4126@gmail.com>
2023-07-21 15:45:21 +0900

    red: NumberConverterのユニットテストを直す

--- ts/src/numberConverter.ts
+++ ts/src/numberConverter.ts
@@ -16,12 +16,12 @@ export class NumberConverter {
 }
 
 if (import.meta.vitest) {
-  const { CyclicNumberRule } = await import(
-    '../src/replaceRules/cyclicNumberRule'
-  )
-  const { PassThroughRule } = await import(
-    '../src/replaceRules/passThroughRule'
-  )
+  // const { CyclicNumberRule } = await import(
+  //   '../src/replaceRules/cyclicNumberRule'
+  // )
+  // const { PassThroughRule } = await import(
+  //   '../src/replaceRules/passThroughRule'
+  // )
 
   describe('NumberConverter', () => {
     describe('Unit Test', () => {
@@ -37,77 +37,93 @@ if (import.meta.vitest) {
         describe('ruleあり', () => {
           describe('ruleが1つ', () => {
             const mockRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue('Replaced'),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(
+                (_, target) => target + 'Replaced'
+              ),
             }
 
-            const fizzBuzz = new NumberConverter([mockRule])
+            const converter = new NumberConverter([mockRule])
 
             it('ルールで指定した文字が返る', () => {
-              expect(fizzBuzz.convert(1)).toBe('Replaced')
+              expect(converter.convert(1)).toBe('Replaced')
             })
           })
 
           describe('ruleが2つ', () => {
             const mockFizzRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue('Fizz'),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(
+                (_, target) => target + 'Fizz'
+              ),
             }
             const mockBuzzRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue('Buzz'),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(
+                (_, target) => target + 'Buzz'
+              ),
             }
 
-            const fizzBuzz = new NumberConverter([mockFizzRule, mockBuzzRule])
+            const converter = new NumberConverter([mockFizzRule, mockBuzzRule])
 
             it('ルールで指定した文字が返る', () => {
-              expect(fizzBuzz.convert(1)).toBe('FizzBuzz')
+              expect(converter.convert(1)).toBe('FizzBuzz')
             })
           })
 
           describe('ruleが3つ', () => {
             const mockFizzRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue(''),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(
+                (_, target) => target + 'Fizz'
+              ),
             }
             const mockBuzzRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue(''),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(
+                (_, target) => target + 'Buzz'
+              ),
             }
             const mockThroughRule: ReplaceRule = {
-              replace: vi.fn<[number], string>().mockReturnValue('1'),
+              match: vi.fn<[number], boolean>((n) => n === 1),
+              apply: vi.fn<[number, string], string>(() => '1'),
             }
 
-            const fizzBuzz = new NumberConverter([
+            const converter = new NumberConverter([
               mockFizzRule,
               mockBuzzRule,
               mockThroughRule,
             ])
 
             it('ルールで指定した文字が返る', () => {
-              expect(fizzBuzz.convert(1)).toBe('1')
+              expect(converter.convert(1)).toBe('1')
             })
           })
         })
       })
     })
 
-    describe('Integration Test', () => {
-      const fizzRule = new CyclicNumberRule(3, 'Fizz')
-      const buzzRule = new CyclicNumberRule(5, 'Buzz')
-      const passThroughRule = new PassThroughRule()
-      const fizzBuzz = new NumberConverter([
-        fizzRule,
-        buzzRule,
-        passThroughRule,
-      ])
+    // describe('Integration Test', () => {
+    //   const fizzRule = new CyclicNumberRule(3, 'Fizz')
+    //   const buzzRule = new CyclicNumberRule(5, 'Buzz')
+    //   const passThroughRule = new PassThroughRule()
+    //   const fizzBuzz = new NumberConverter([
+    //     fizzRule,
+    //     buzzRule,
+    //     passThroughRule,
+    //   ])
 
-      it('FizzBuzz', () => {
-        expect(fizzBuzz.convert(1)).toBe('1')
-        expect(fizzBuzz.convert(2)).toBe('2')
-        expect(fizzBuzz.convert(3)).toBe('Fizz')
-        expect(fizzBuzz.convert(4)).toBe('4')
-        expect(fizzBuzz.convert(5)).toBe('Buzz')
-        expect(fizzBuzz.convert(6)).toBe('Fizz')
-        expect(fizzBuzz.convert(10)).toBe('Buzz')
-        expect(fizzBuzz.convert(15)).toBe('FizzBuzz')
-        expect(fizzBuzz.convert(30)).toBe('FizzBuzz')
-      })
-    })
+    //   it('FizzBuzz', () => {
+    //     expect(fizzBuzz.convert(1)).toBe('1')
+    //     expect(fizzBuzz.convert(2)).toBe('2')
+    //     expect(fizzBuzz.convert(3)).toBe('Fizz')
+    //     expect(fizzBuzz.convert(4)).toBe('4')
+    //     expect(fizzBuzz.convert(5)).toBe('Buzz')
+    //     expect(fizzBuzz.convert(6)).toBe('Fizz')
+    //     expect(fizzBuzz.convert(10)).toBe('Buzz')
+    //     expect(fizzBuzz.convert(15)).toBe('FizzBuzz')
+    //     expect(fizzBuzz.convert(30)).toBe('FizzBuzz')
+    //   })
+    // })
   })
 }
